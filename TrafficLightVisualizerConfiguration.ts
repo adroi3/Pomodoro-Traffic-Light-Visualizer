d2a995f6ac89a93d31fd3d1fa2fefa30c9eccadf
Hedtke, David <D.Hedtke@vescon.com>
2018-12-03 18:36:25 +0100

    Updated config

--- TrafficLightVisualizerConfiguration.ts
+++ TrafficLightVisualizerConfiguration.ts
@@ -1,7 +1,7 @@
 export class TrafficLightVisualizerConfiguration {
     public serviceOptions = {
-        timeoutForReachingYellowInMilliseconds: 20 * 1000 * 60,
-        timeoutForReachingGreenInMilliseconds: 5 * 1000 * 60,
+        timeoutForReachingYellowInMilliseconds: 10000, //20 * 1000 * 60,
+        timeoutForReachingGreenInMilliseconds: 5000, //5 * 1000 * 60,
         port: "COM4",
         baudRate: 115200,
         pomodoroIsOverMessage: "Pomodoro is over",
