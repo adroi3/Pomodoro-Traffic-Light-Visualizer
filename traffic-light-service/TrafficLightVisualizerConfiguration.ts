08d92f5a3ad8432a08a6eb5d1a7f40bdf615bbd6
Hedtke, David <D.Hedtke@vescon.com>
2019-04-29 10:24:27 +0200

    Changed config

--- traffic-light-service/TrafficLightVisualizerConfiguration.ts
+++ traffic-light-service/TrafficLightVisualizerConfiguration.ts
@@ -1,14 +1,14 @@
 export class TrafficLightVisualizerConfiguration {
     public serviceOptions = {
-        timeoutForReachingYellowInMinutes: 0.1,
-        timeoutForReachingGreenInMinutes: 0.1,
-        timeoutForReachingEndOfBreakInMinutes: 0.1,
+        timeoutForReachingYellowInMinutes: 20,
+        timeoutForReachingGreenInMinutes: 5,
+        timeoutForReachingEndOfBreakInMinutes: 5,
         timeoutForStayingGreenWhenBreakEndedInSeconds: 10,
         port: "COM4",
         baudRate: 115200,
         pomodoroIsAlmostOverMessage: "Pomdoro is almost over",
         pomodoroIsOverMessage: "Pomodoro is over",
         breakIsOverMessage: "Break is over",
-        withTrafficLightPlugin: false,
+        withTrafficLightPlugin: true,
     }
 }
