060a5e979201240818ab584e8660fcc54309b303
Hedtke, David <D.Hedtke@vescon.com>
2019-06-03 10:11:34 +0200

    Cleaned up

--- ts_output/src/Api/TrafficLightVisualizerPlugins/TrafficLightStatus.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var TrafficLightStatus;
-(function (TrafficLightStatus) {
-    TrafficLightStatus[TrafficLightStatus["Red"] = 0] = "Red";
-    TrafficLightStatus[TrafficLightStatus["Yellow"] = 1] = "Yellow";
-    TrafficLightStatus[TrafficLightStatus["Green"] = 2] = "Green";
-})(TrafficLightStatus = exports.TrafficLightStatus || (exports.TrafficLightStatus = {}));
-//# sourceMappingURL=TrafficLightStatus.js.map
--- ts_output/src/Plugins/TrafficLightVisualizerPlugin/TrafficLightVisualizerPluginNamespace.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var ArduinoPlugin_1 = require("./ArduinoPlugin");
-exports.ArduinoPlugin = ArduinoPlugin_1.ArduinoPlugin;
-//# sourceMappingURL=TrafficLightVisualizerPluginNamespace.js.map
